964e1bf5d530e0f2ac2819d9632bf2fc131d3021
yusukeee811 <132319174+yusukeee811@users.noreply.github.com>
2023-05-05 01:53:51 +0000

    [Update]

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -9,13 +9,15 @@ class ListsController < ApplicationController
     # 3. データをデータベースに保存するためのsaveメソッド実行
     list.save
     # 4. トップ画面へリダイレクト
-    redirect_to '/top'
+    redirect_to list_path(list.id)
   end
 
   def index
+    @lists = List.all
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
